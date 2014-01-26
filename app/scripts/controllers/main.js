76d86dbb3a2c095965abf393a846d7d9b5626ff7
Vasiliy Kevroletin <kevroletin@gmail.com>
2014-01-26 15:31:44 +1100

    Generate and store random username into cookies
    Random username will be used to implement "hide" feature which is
    replacement to delete.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,13 +3,27 @@
 define(['underscore', 'app', 'firebase', 'angularfire'],
 	   function(_, app, Firebase)
 {
-	app.controller('MainCtrl', function ($scope, $firebase, $http, $log, $document, $timeout, firebaseCollection) {
+	app.constant('firebaseUrl', 'https://picture-board.firebaseio.com/');
+	app.controller('MainCtrl', function ($scope, $firebase, $http, $log, $document, $timeout, $cookies, firebaseCollection, firebaseUrl) {
+		$scope.generateUsername = function() {
+			var alph = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' +
+				       'abcdefghijklmnopqrstuvwxyz' +
+				       '0123456789';
+			$scope.username = _(_.sample(alph, 15)).join('');
+			$cookies.username = $scope.username;
+			$scope.firstTimeHere = true;
+		};
+		if ($cookies.username) {
+			$scope.username = $cookies.username;
+		} else {
+			$scope.generateUsername();
+		}
+
 		$scope.currentPage = 0;
 		$scope.pageSize = 30;
 		$scope.showForm = true;
-		$scope.index = $firebase(new Firebase('https://picture-board.firebaseio.com/index'));
-		// TODO: init $scope.index
-		$scope.posts = firebaseCollection('https://picture-board.firebaseio.com/test');
+		$scope.index = $firebase(new Firebase(firebaseUrl + 'index'));
+		$scope.posts = firebaseCollection(firebaseUrl + 'test');
 		$scope.addNewPost = function() {
 			if ($scope.new) {
 				$scope.new.i = $scope.getIndex();
@@ -23,6 +37,9 @@ define(['underscore', 'app', 'firebase', 'angularfire'],
 		};
 		$scope.getIndex = function() {
 			var res = $scope.index.$value;
+			if ( !res ) {
+				res = 1;
+			}
 			$scope.index.$set(res + 1);
 			return res;
 		};
